cfde1f20df9170ddbe5ed96873303d539da1e0ee
Gaetan Lepage <gaetan@glepage.com>
2025-11-21 15:56:32 +0100

    rename array files

--- acoustix/microphone_array/__init__.py
+++ acoustix/microphone_array/__init__.py
@@ -1,8 +1,8 @@
 from .array import MicArray, compute_ild_ipd_from_stft
 from .binaural_array import BinauralArray
 from .mono_array import MonoArray
-from .quad_array import SquareArray
-from .triangle import TriangleArray
+from .square_array import SquareArray
+from .triangle_array import TriangleArray
 from .uniform_linear_array import UniformLinearArray
 
 __all__ = [
