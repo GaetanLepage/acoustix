cb249c4d37f5ade851dc4e0112eb74b76e180c08
Gaetan Lepage <gaetan@glepage.com>
2025-11-25 19:51:22 +0100

    test/room/test_rir: try to debug

--- tests/room/test_rir.py
+++ tests/room/test_rir.py
@@ -4,17 +4,17 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-from acoustix.room import Source
+from acoustix.room import Source, SpeechSource
 from acoustix.room.gpu_rir_room import GpuRirRoom
 
-N_REPETITIONS: int = 2
+N_REPETITIONS: int = 100
 
 
 def test_rir() -> None:
     room: GpuRirRoom = GpuRirRoom()
     room.init_grid(add_grid_mics=True)
 
-    source: Source = Source(
+    source: Source = SpeechSource(
         name="source",
         position=np.array(
             [6, 3, 1.5],
@@ -25,22 +25,26 @@ def test_rir() -> None:
         clip_to_grid=True,
     )
 
+    rir_len: int = 4_800
+    n_mics: int = len(room.microphones)
     rir_tensor: np.ndarray = np.zeros(
         shape=(
             N_REPETITIONS,
-            112,
-            4800,
+            n_mics,
+            rir_len,
         ),
     )
 
     for rep_index in range(N_REPETITIONS):
+        source.load_signal()
         room.rir_up_to_date = False
 
         room.pre_compute_rir()
+        assert room.rir.squeeze().shape == (n_mics, rir_len)
         rir_tensor[rep_index] = room.rir.squeeze()
 
     rir_mic_50: np.ndarray = rir_tensor[:, 50, :].squeeze()
-    print(rir_mic_50.shape)
+    print("RIR shape:", rir_mic_50.shape)
 
     fig, axes = matplotlib.pyplot.subplots()
     # print('min:', rir_tensor.min())
@@ -58,3 +62,4 @@ def test_rir() -> None:
     print(mean_over_reps.shape)
 
     plt.show()
+    plt.close()
